f72d1ca328d1dc697dc4b943c5fbb78b2aef6641
Elena Pavel <elena.pavel.dev@gmail.com>
2019-11-21 12:15:58 +0200

    Removed component.

--- components/View/PageView/index.js
+++ components/View/PageView/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "~/components/Header";
-import Bubble from "~/components/Bubble";
 import Gradient from "~/components/Gradient";
 
 import $, { css } from "./style.css";
