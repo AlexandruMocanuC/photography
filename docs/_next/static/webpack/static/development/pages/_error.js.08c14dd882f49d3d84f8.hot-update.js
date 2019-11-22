3414021207062c29dda3fdf594f7b0ef4d98ad0a
Elena Pavel <elena.pavel.dev@gmail.com>
2019-11-22 07:30:36 +0200

    Compiled files.

---
+++ docs/_next/static/webpack/static/development/pages/_error.js.08c14dd882f49d3d84f8.hot-update.js
@@ -0,0 +1,52 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "./components/Menu/MenuItem/index.js":
+/*!*******************************************!*\
+  !*** ./components/Menu/MenuItem/index.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _playground_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ~/playground.js */ "./playground.js");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./style.css */ "./components/Menu/MenuItem/style.css");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_4__);
+var _jsxFileName = "/Users/elena/projects/amc/components/Menu/MenuItem/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function (_ref) {
+  var isSubItem = _ref.isSubItem,
+      view = _ref.view,
+      isSelected = _ref.isSelected,
+      _ref$onSelect = _ref.onSelect,
+      onSelect = _ref$onSelect === void 0 ? function () {} : _ref$onSelect;
+  return __jsx("b", {
+    className: "".concat(_style_css__WEBPACK_IMPORTED_MODULE_4___default.a.item, " ").concat(isSelected ? _style_css__WEBPACK_IMPORTED_MODULE_4___default.a.is_selected : "", " ").concat(isSubItem ? _style_css__WEBPACK_IMPORTED_MODULE_4___default.a.sub_menu : ""),
+    onClick: function onClick() {
+      next_router__WEBPACK_IMPORTED_MODULE_2___default.a.push("".concat(_playground_js__WEBPACK_IMPORTED_MODULE_3__["BASE"], "/"), "".concat(_playground_js__WEBPACK_IMPORTED_MODULE_3__["BASE"], "/").concat(view == "home" ? "" : view), {
+        shallow: true
+      });
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, view);
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.08c14dd882f49d3d84f8.hot-update.js.map
