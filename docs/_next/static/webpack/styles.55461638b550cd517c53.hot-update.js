b4bc62190edd103cc4327d76aa5b5d9827777d26
Elena Pavel <elena.pavel.dev@gmail.com>
2020-05-14 16:18:43 +0300

    Compiled files.

---
+++ docs/_next/static/webpack/styles.55461638b550cd517c53.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("styles",{
+
+/***/ "./components/DefaultContext/appStyle.css":
+false,
+
+/***/ "./components/DefaultContext/mobileAppStyle.css":
+false,
+
+/***/ 6:
+false,
+
+/***/ 9:
+false
+
+})
---
+++ docs/_next/static/webpack/styles.6d33e072d0034ee6a0e8.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("styles",{
+
+/***/ "./views/home/App/style.css":
+false,
+
+/***/ "./views/home/MobileApp/style.css":
+false,
+
+/***/ 1:
+false,
+
+/***/ 7:
+false
+
+})
